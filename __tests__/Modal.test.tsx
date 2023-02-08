cfe0b268c4359eea52de86947c856803da9720e9
Aaron <22418751+ajvsol@users.noreply.github.com>
2023-02-08 16:31:59 +0000

    more tests wip

--- __tests__/Modal.test.tsx
+++ __tests__/Modal.test.tsx
@@ -1,15 +1,16 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { it, expect, describe } from "@jest/globals";
 import {
   exampleData,
   StateContextProvider,
+  useStateContext,
 } from "../context/StateContextProvider";
 import Modal from "../components/Modal";
 
-describe("Modal is working correctly", () => {
+describe("Modal", () => {
   const searchResults = exampleData;
 
-  it(`Modal renders on the homepage`, () => {
+  it(`renders on the homepage`, () => {
     render(
       <StateContextProvider
         value={{
@@ -27,4 +28,47 @@ describe("Modal is working correctly", () => {
     const actual = screen.getByTestId("Modal");
     expect(actual).toBeInTheDocument();
   });
+
+  //it(`renders the correct text on the modal`, () => {
+  //  render(
+  //    <StateContextProvider
+  //      value={{
+  //        searchResults,
+  //        setSearchResults: jest.fn(),
+  //        isOpen: true,
+  //        setIsOpen: jest.fn(),
+  //        modalIndex: 0,
+  //        setModalIndex: jest.fn(),
+  //      }}
+  //    >
+  //      <Modal />
+  //    </StateContextProvider>
+  //  );
+  //  expect(
+  //    screen.getByText(searchResults.articles[0].title)
+  //  ).toBeInTheDocument();
+  //  expect(
+  //    screen.getByText(searchResults.articles[0].content)
+  //  ).toBeInTheDocument();
+  //});
+
+  //it(`closes the modal by clicking the close button`, () => {
+  //  render(
+  //    <StateContextProvider
+  //      value={{
+  //        searchResults,
+  //        setSearchResults: jest.fn(),
+  //        isOpen: true,
+  //        setIsOpen: jest.fn(),
+  //        modalIndex: 0,
+  //        setModalIndex: jest.fn(),
+  //      }}
+  //    >
+  //      <Modal />
+  //    </StateContextProvider>
+  //  );
+  //  const closeModalButton = screen.getByText("Read Article");
+  //  fireEvent.click(closeModalButton);
+  //  expect(useStateContext().setIsOpen).toHaveBeenCalledWith(false);
+  //});
 });
